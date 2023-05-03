842bcdafc037b590d7455c3e3821c9928e9d08e8
JeongMin <skawjdals96@gmail.com>
2023-05-03 19:49:39 +0900

    Feat: Authentication객체가 가질 수 있는 2가지 타입으로 조회 메서드 생성

--- security/src/main/java/com/study/security/controller/IndexController.java
+++ security/src/main/java/com/study/security/controller/IndexController.java
@@ -1,16 +1,21 @@
 package com.study.security.controller;
 
 import com.study.security.Repository.UserRepository;
+import com.study.security.config.auth.PrincipalDetails;
 import com.study.security.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +24,32 @@ public class IndexController {
     private final UserRepository userRepository;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 일반 로그인
+    @GetMapping("/test/login")
+    public @ResponseBody
+    String testLogin(Authentication authentication,
+                     @AuthenticationPrincipal PrincipalDetails userDetails) { //DI(의존성 주입)
+        System.out.println("/test/login =======");
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("authentication = " + principalDetails.getUser());
+
+        System.out.println("userDetails = " + userDetails.getUser());
+        return "세션 정보 확인하기";
+    }
+
+    // oauth 로그인
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody
+    String testOAuthLogin(Authentication authentication,
+                          @AuthenticationPrincipal OAuth2User oauth) { //DI(의존성 주입)
+        System.out.println("/test/login =======");
+        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication = " + oauth2User.getAttributes());
+        System.out.println("oauth2User = " + oauth.getAttributes());
+
+        return "OAuth 세션 정보 확인하기";
+    }
 
     @GetMapping("/")
     public String index() {
