7022e77866759f4db7a22f0bf404b3355034e5cd
JeongMin <skawjdals96@gmail.com>
2023-05-03 17:24:10 +0900

    Feat: oauth2 구글 로그인 설정 추가

--- security/src/main/java/com/study/security/config/SecurityConfig.java
+++ security/src/main/java/com/study/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.study.security.config;
 
+import com.study.security.config.oauth.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.security.Principal;
+
+// 1. 코드받기(인증) 2.엑세스토큰(권한) 3.사용자프로필 정보를 가져옴
+// 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+// 4-2. 하지만 추가적인 정보(집주소나 등급)가 필요할 수도 있다.
+
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터 체인에 등록 된다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) //secured 애노테이션 활성화, preAuthorize, postAuthorize 애노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PrincipalOauth2UserService principalOauth2UserService;
 
     //해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     @Bean
@@ -31,7 +42,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") //login 주소가 호출이 되면 시큐리리티가 낚아채 대신 로그인을 진행해준다.
-                .defaultSuccessUrl("/");
-
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm") //구글 로그인이 완료된 뒤의 후 처리가 필요.
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService); // Tip. 코드로 받지 않고 엑세스 토큰과 사용자 프로필 정보를 받는다.
     }
 }
