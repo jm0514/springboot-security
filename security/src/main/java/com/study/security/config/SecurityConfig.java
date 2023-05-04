2ec9a4a7bad01e6a7c8532105c79fa6fa313916e
JeongMin <skawjdals96@gmail.com>
2023-05-04 18:43:37 +0900

    Refactor: 순환 참조현상 해결

--- security/src/main/java/com/study/security/config/SecurityConfig.java
+++ security/src/main/java/com/study/security/config/SecurityConfig.java
@@ -2,33 +2,32 @@ package com.study.security.config;
 
 import com.study.security.config.oauth.PrincipalOauth2UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
-import java.security.Principal;
 
 // 1. 코드받기(인증) 2.엑세스토큰(권한) 3.사용자프로필 정보를 가져옴
 // 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
 // 4-2. 하지만 추가적인 정보(집주소나 등급)가 필요할 수도 있다.
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터 체인에 등록 된다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) //secured 애노테이션 활성화, preAuthorize, postAuthorize 애노테이션 활성화
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PrincipalOauth2UserService principalOauth2UserService;
 
     //해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
-    @Bean
-    public BCryptPasswordEncoder encoderPwd() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public BCryptPasswordEncoder encoderPwd() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
