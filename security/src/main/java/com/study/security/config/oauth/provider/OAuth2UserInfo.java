a48f8fc043d2d381f860cdeca3ba33aa59defafa
JeongMin <skawjdals96@gmail.com>
2023-05-04 22:35:43 +0900

    Feat: 각 oauth 로그인마다 다르게 필요한 정보들을 재정의

---
+++ security/src/main/java/com/study/security/config/oauth/provider/OAuth2UserInfo.java
@@ -0,0 +1,11 @@
+package com.study.security.config.oauth.provider;
+
+public interface OAuth2UserInfo {
+    String getProviderId();
+
+    String getProvider();
+
+    String getEmail();
+
+    String getName();
+}
