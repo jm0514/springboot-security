b867dd8aaa585f66d835f1701cf392fe75f7b99a
JeongMin <skawjdals96@gmail.com>
2023-05-04 18:40:12 +0900

    Feat: OAuth2User 구현

--- security/src/main/java/com/study/security/config/auth/PrincipalDetails.java
+++ security/src/main/java/com/study/security/config/auth/PrincipalDetails.java
@@ -9,18 +9,36 @@ package com.study.security.config.auth;
 //Security Session -> Authentication -> UserDetails -> UserDetails(PrincipalDetails)
 
 import com.study.security.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user; //컴포지션
 
+    private Map<String, Object> attributes;
+
+    //일반 로그인
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    // OAuth 로그인
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
     }
 
     //해당 User의 권한을 리턴하는 곳
@@ -67,4 +85,10 @@ public class PrincipalDetails implements UserDetails {
         //현재시간 - 로긴시간 -> 1년 초과하면 return false;
        return true;
     }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
 }
