712069277d528eb69d32b005053bb8f8e7d5f3a4
JeongMin <skawjdals96@gmail.com>
2023-05-04 18:27:22 +0900

    Feat: /user 경로를 통해서 PrincipalDetails의 정보 받는 메서드 추가

--- security/src/main/java/com/study/security/controller/IndexController.java
+++ security/src/main/java/com/study/security/controller/IndexController.java
@@ -56,9 +56,12 @@ public class IndexController {
         return "index";
     }
 
+    // OAuth 로그인을 해도 PrincipalDetails
+    // 일반 로그인을 해도 PrincipalDetails
     @GetMapping("/user")
     public @ResponseBody
-    String user() {
+    String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println("principalDetails = " + principalDetails.getUser());
         return "user";
     }
 
